500d5bbf55e5ebede24c306b4132fc9c344d6c93
Gbuomprisco <giancarlopsk@gmail.com>
2017-07-24 19:02:36 +0100

    added maps regexes

--- src/regexes.ts
+++ src/regexes.ts
@@ -1,3 +1,5 @@
 export const REGEX = /\$([\S]+):\s([\S\s]+);/;
 export const SELECTOR_REGEX = /\b([\S.]+)-selector/;
 export const PROP_REGEX = /prop:\s([\S\s]+);/;
+export const MAP_REGEX = /(\$\S+):\s*\(\n([\s\S]+)\);/;
+export const MAP_LINE_REGEX = /(\$\S+):\s*\(\n\s(\S+):\s(.+)(?:,)/;
