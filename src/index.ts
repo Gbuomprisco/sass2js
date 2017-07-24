a130742f16d10680f68b3ffc5815f7c68bcd340c
gbuomprisco <giancarlopsk@gmail.com>
2017-07-24 16:13:38 +0100

    Minor refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
 import compile from './compile';
 import extract from './extractor';
-
 import { processSass } from './helpers';
 
 export = async function(source: string): Promise<object | undefined> {
-    const sass = await compile(extract(source));
+    const compiled = await compile(extract(source));
 
     try {
-        return processSass(sass);
+        return processSass(compiled);
     } catch(e) {
         throw new Error(e);
     }
