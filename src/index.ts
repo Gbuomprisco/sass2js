24280879097a18d91842fe7c95fd3b876a9922b9
gbuomprisco <giancarlopsk@gmail.com>
2017-07-24 16:16:59 +0100

    Await to be inside try/catch

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import compile from './compile';
-import extract from './extractor';
+import extract from './extract';
 import { processSass } from './helpers';
 
 export = async function(source: string): Promise<object | undefined> {
-    const compiled = await compile(extract(source));
-
     try {
+        const compiled = await compile(extract(source));
+
         return processSass(compiled);
     } catch(e) {
         throw new Error(e);
